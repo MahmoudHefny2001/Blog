e6d0aa56045dca11168faf5ab58798c659d94d3f
MahmoudHefny2001 <hefny4@gmail.com>
2023-03-24 23:55:24 +0200

    CSRF trusted origins

--- django/core/settings.py
+++ django/core/settings.py
@@ -171,6 +171,7 @@ REST_FRAMEWORK = {
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000/api/admin/create/',
     'https://hefnyspace.onrender.com',
+    "https://hefnyspace.ninja",
 ]
 
 CORS_ALLOWED_ORIGINS = [
