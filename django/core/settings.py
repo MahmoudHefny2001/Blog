992374c578aa4ffe07fd3fb07fe6abcc0f754342
MahmoudHefny2001 <hefny4@gmail.com>
2023-03-24 06:02:34 +0200

    Images

--- django/core/settings.py
+++ django/core/settings.py
@@ -201,11 +201,13 @@ CORS_ORIGIN_WHITELIST = [
 
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://127.0.0.1:8000/api/",
     "http://localhost:8080",
     "http://192.168.0.222:8080",
     
     "https://hefny-space.onrender.com",
     "https://hefnyspace.ninja/",
+    "http://hefnyspace.ninja/"
 ]
 
 CORS_ALLOW_HEADERS = "access-control-allow-origin"
