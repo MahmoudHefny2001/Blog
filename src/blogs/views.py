d03690907906da7cf5d21dfa6fbb8be654aaa93c
MahmoudHefny2001 <hefny4@gmail.com>
2024-07-24 05:56:35 +0300

    Done - All CRUD operations on blogs for authenticated users only with permissions and Containerized

--- src/blogs/views.py
+++ src/blogs/views.py
@@ -20,8 +20,8 @@ class BlogViewSet(ModelViewSet):
     authentication_classes = [TokenAuthentication]
 
     def get_permissions(self):
-        if self.action in SAFE_METHODS:
-            return [AllowAny()]
+        if self.action == 'list' or self.action == 'retrieve':
+            return [AllowAny(),]
         else:
             return [
                 IsAuthenticated(), 
@@ -47,7 +47,6 @@ class BlogViewSet(ModelViewSet):
 
     def get_queryset(self):
         return super().get_queryset().order_by('-created')
-    
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
